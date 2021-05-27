c99b6d897c586bcc38bcda89bdde7724d62f3b2a
Lins Mariya <linzmar0002@gmail.com>
2021-05-27 11:08:46 +0530

    Update1

--- patterns.c
+++ patterns.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include<lcs50.h>
 void main()
 {
 int option,rows,i,j;
 printf("Enter the type of pattern  ");
-scanf("%d",&option);
+gets("%d",&option);
 if(option==1)
 {
 printf("enter the rows  ");
-scanf("%d",&rows);
+gets("%d",&rows);
 for(i=rows;i>0;i--)
 {
 for(j=0;j<i;j++)
@@ -20,7 +21,7 @@ printf("\n");
 else if(option==2)
 {
 printf("enter the rows  ");
-scanf("%d",&rows);
+gets("%d",&rows);
 for(i=1;i<=rows;i++)
 {
 for(j=0;j<i;j++)
