ea17d5d482431c7b6b4392f3ff8377d99ff984e9
Lins Mariya <linzmar0002@gmail.com>
2021-05-27 11:24:18 +0530

    Removed printing stetement

--- patterns.c
+++ patterns.c
@@ -3,10 +3,10 @@
 void main()
 {
 int option,rows,i,j;
-option=get_int("Enter the type of pattern  ");
+option=get_int("");
 if(option==1)
 {
-rows=get_int("enter the rows  ");
+rows=get_int("");
 printf("\n");
 for(i=rows;i>0;i--)
 {
@@ -19,7 +19,7 @@ printf("\n");
 }
 else if(option==2)
 {
-rows=get_int("enter the rows  ");
+rows=get_int("");
   printf("\n");
 for(i=1;i<=rows;i++)
 {
